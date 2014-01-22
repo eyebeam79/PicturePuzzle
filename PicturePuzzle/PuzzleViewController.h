37580455ceef2c74d5c5823193d7897ca3862451
SDT1 <eyebeam79@naver.com>
2014-01-22 10:28:34 +0900

    생성된 프로젝트의 이니셜 커밋입니다.

---
+++ PicturePuzzle/PuzzleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PuzzleViewController.h
+//  PicturePuzzle
+//
+//  Created by SDT1 on 2014. 1. 22..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PuzzleViewController : ViewController
+
+@end
---
+++ PicturePuzzle/PuzzleViewController.m
@@ -0,0 +1,38 @@
+//
+//  PuzzleViewController.m
+//  PicturePuzzle
+//
+//  Created by SDT1 on 2014. 1. 22..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import "PuzzleViewController.h"
+
+@interface PuzzleViewController ()
+
+@end
+
+@implementation PuzzleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
